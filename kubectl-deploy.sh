d85cac1babdf5d320845946f50e896fba9b37a4c
Theotime Leveque <theotime.leveque@dailymotion.com>
2016-10-12 16:17:25 -0400

    Travis: Remove duplicate env var.

--- kubectl-deploy.sh
+++ kubectl-deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  gcloud container clusters get-credentials $PROJECT_ID
+  gcloud container clusters get-credentials $CLOUDSDK_CORE_PROJECT
   kubectl apply -f config/k8s/mongo.yml
-  kubectl apply -f config/k8s/$PROJECT_ID.yml
-  kubectl rolling-update web-controller --image=gcr.io/$PROJECT_ID/$PROJECT_ID:v1 --image-pull-policy Always
+  kubectl apply -f config/k8s/$CLOUDSDK_CORE_PROJECT.yml
+  kubectl rolling-update web-controller --image=gcr.io/$CLOUDSDK_CORE_PROJECT/$CLOUDSDK_CORE_PROJECT:v1 --image-pull-policy Always
 fi
