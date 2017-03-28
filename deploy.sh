a4ef09091724f36cd884b5dacffd3fe9686614d9
Theotime Leveque <theotime.leveque@dailymotion.com>
2017-03-27 22:12:33 -0400

    Travis: Fix build.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   gcloud --quiet components update kubectl
   # Push to Google container registry
   docker build -t gcr.io/$CLOUDSDK_CORE_PROJECT/$CLOUDSDK_CORE_PROJECT:v1 .
-  gcloud docker push gcr.io/$CLOUDSDK_CORE_PROJECT/$CLOUDSDK_CORE_PROJECT:v1 > /dev/null
+  gcloud docker -- push gcr.io/$CLOUDSDK_CORE_PROJECT/$CLOUDSDK_CORE_PROJECT:v1 > /dev/null
   # Deploy to the cluster
   gcloud container clusters get-credentials $CLOUDSDK_CORE_PROJECT
   kubectl apply -f config/k8s/mongo.yml
